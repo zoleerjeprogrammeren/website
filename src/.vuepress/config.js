6109333496dfb6a631b7e6c249fc10d1d9681e95
ronniew <rwaebers@gmail.com>
2021-10-30 10:51:13 +0200

    :tada: initial commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,81 @@
+const { description } = require("../../package");
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: "ZoLeerJeProgrammeren",
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: "",
+    editLinks: false,
+    docsDir: "",
+    editLinkText: "",
+    lastUpdated: false,
+    nav: [
+      {
+        text: "HTML",
+        link: "/html/",
+      },
+      {
+        text: "CSS",
+        link: "/css/",
+      },
+      {
+        text: "JavaScript",
+        link: "/javascript/",
+      },
+    ],
+    sidebar: {
+      "/html/": [
+        {
+          title: "HTML",
+          collapsable: false,
+          children: [""],
+        },
+      ],
+      "/css/": [
+        {
+          title: "CSS",
+          collapsable: false,
+          children: [""],
+        },
+      ],
+      "/javascript/": [
+        {
+          title: "JavaScript",
+          collapsable: false,
+          children: [""],
+        },
+      ],
+    },
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+};
